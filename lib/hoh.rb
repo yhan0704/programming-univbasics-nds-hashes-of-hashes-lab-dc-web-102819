03617c6d4bfed7c38fb0471ef45646367dece465
Young Han <han20@umbc.edu>
2019-10-22 03:14:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,7 +48,13 @@ def naming_system
     :sub_category=>{
       :label=> "Class",
     :sub_category=>{
-      :label=> "Order"
+      :label=> "Order",
+      :sub_category=>{
+        :label=> "Family",
+        :sub_category=>{
+          :label=> "Genus"
+        }
+      }
     }
     }
   }
